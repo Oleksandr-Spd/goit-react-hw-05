ee12b4a55e47e0a855bb8232f49601a008fe322d
Oleksandr-Spd <133060544+Oleksandr-Spd@users.noreply.github.com>
2024-02-27 00:42:14 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <NavLink>Home</NavLink>
+        <NavLink>Movies</NavLink>
+      </nav>
+    </>
+  );
+}
+
+export default App;
